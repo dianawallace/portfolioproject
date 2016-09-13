28fde202395c2464844b3f7ff9f7cae51e7024ee
Diana Wallace <dianawallacedeveloper@gmail.com>
2016-09-13 23:36:14 +0000

    testing array in works.js file added different images

--- js/work.js
+++ js/work.js
@@ -1,14 +1,13 @@
 
+ var works = [
 
+ "https://www.petfinder.com/wp-content/uploads/2012/09/Blog-Kitty-Cam-solo.jpg",
 
-    var works = [
-  "images/shopping_list.png",
-    
-  "images/quiz_app.png",
-    
-  "images/behance_api.png",
-  
-  "images/stackoverflow_ajax.png"
-  ] 
+ "http://www.animal-photography.com/thumbs/red_tabby_long_hair_kitten_~AP-0UJFTC-TH.jpg",
 
+ "http://www.animal-photography.com/thumbs/silver_tabby_kittens~AP-0JO6Y9-TH.jpg",
+
+ "http://www.animal-photography.com/thumbs/silver_tabby_kitten_looking_up~AP-0DLVMB-TH.jpg"
+
+]
    
