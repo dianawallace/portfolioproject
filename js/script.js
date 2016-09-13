a0271390b11a45d7edc6156f3c364ab0ac149570
Diana Wallace <dianawallacedeveloper@gmail.com>
2016-09-13 23:22:28 +0000

    deleted works array from script.js to see if works in work.js

--- js/script.js
+++ js/script.js
@@ -80,24 +80,7 @@ $(document).ready(function($){
           }
         });
         
-     var works = [
-       {
-        title: "First Project",
-        pic: "images/shopping_list.png"
-       },
-       {
-        title: "Second Project",
-        pic: "images/quiz_app.png"
-       },
-       {
-        title: "Third Project",
-        pic: "images/behance_api.png"
-       },
-       {  
-        title: "Fourth Project",
-        pic: "images/stackoverflow_ajax.png"
-       },
-  ]; 
+   
         
   // Work section
     for (var i = 0; i < works.length; ++i ) {
